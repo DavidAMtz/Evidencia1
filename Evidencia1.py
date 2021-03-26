6017ea0de7b21ba50cbec039e3785b084c2c2473
David Adrian Martinez Saldaña <davidadrian272019@hotmail.com>
2021-03-26 11:24:17 -0600

    Se agrego la función de registroVentas con el diccionario ventas

---
+++ Evidencia1.py
@@ -0,0 +1,20 @@
+ventas = {}
+
+def registroVentas():
+    respuesta = 1
+    while respuesta == 1:
+        claveCompra = int(input("Ingresa una clave de compra: "))
+        try:
+            if claveCompra in ventas:
+                print ("ESA CLAVE YA ESTA REGISTRADA INTENTE CON OTRA")
+                claveCompra = int(input("Ingresa una clave de compra: "))
+        finally:
+            print ("CLAVE ACEPTADA")
+                        
+        descripcionArticulo = input("¿Cual es la descripción del articulo?: ")
+        cantidadPiezasVendidas = int(input("¿Cual es la cantidad de piezas vendidas?: "))
+        precioDeVenta = int(input("¿Cual es el precio de venta?: "))
+        ventas[claveCompra] = [descripcionArticulo,cantidadPiezasVendidas,precioDeVenta]
+        total = cantidadPiezasVendidas * precioDeVenta
+        print (f"Su total de compra seria de: {total}")
+        respuesta = int(input("¿DESEA REGISTRAR OTRA VENTA?: 1-.SI/2-.NO: "))
